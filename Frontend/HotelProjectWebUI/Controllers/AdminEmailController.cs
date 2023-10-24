0b73c52f28af20ebf2d08044182de9b4b5c874d0
Akif Bilgin <akif.bil@gmail.com>
2023-10-24 21:24:29 +0200

    MimeKit

---
+++ Frontend/HotelProjectWebUI/Controllers/AdminEmailController.cs
@@ -0,0 +1,39 @@
+﻿using HotelProjectWebUI.Models.Mail;
+using MailKit.Net.Smtp;
+using Microsoft.AspNetCore.Mvc;
+using MimeKit;
+
+namespace HotelProjectWebUI.Controllers
+{
+    public class AdminEmailController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(AdminMailViewModel adminMailViewModel)
+        {
+            MimeMessage mimeMessage = new MimeMessage();
+            MailboxAddress mailboxAddressFrom = new MailboxAddress("Admin", "akif45889@gmail.com");
+            mimeMessage.From.Add(mailboxAddressFrom);
+
+            MailboxAddress mailboxAddressTo = new MailboxAddress("User", adminMailViewModel.ReceiverEmail);
+            mimeMessage.To.Add(mailboxAddressTo);
+
+            var bodyBuilder = new BodyBuilder();
+            bodyBuilder.TextBody = adminMailViewModel.Message;
+            mimeMessage.Body=bodyBuilder.ToMessageBody();
+            mimeMessage.Subject = adminMailViewModel.Subject;
+
+            SmtpClient smtpClient = new SmtpClient();
+            smtpClient.Connect("smtp.gmail.com", 587, false);
+            smtpClient.Authenticate("akif45889@gmail.com", "hfmhazxsccocpspk");
+            smtpClient.Send(mimeMessage);
+            smtpClient.Disconnect(true);
+            return View();
+        }
+    }
+}
---
+++ Frontend/HotelProjectWebUI/Models/Mail/AdminMailViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace HotelProjectWebUI.Models.Mail
+{
+    public class AdminMailViewModel
+    {
+        public string Name { get; set; }
+        public string SenderEmail { get; set; }
+        public string ReceiverEmail { get; set; }
+        public string Subject { get; set; }
+        public string Message { get; set; }
+    }
+}
